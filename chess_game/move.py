0fb0acc3f907f9ebc44755a4644ddecd25efa52a
VldKnd <kondratyev.w.i@gmail.com>
2025-03-17 12:31:23 +0100

    feat(chess): finish chess engine

--- chess_game/move.py
+++ chess_game/move.py
@@ -34,6 +34,12 @@ def get_current_fen_position() -> str:
         return file.read()
 
 
+def update_current_fen_position(fen_position: str) -> int:
+    root_path = getenv_or_raise_runtime("ROOT_PATH")
+    with open(f"{root_path}/chess_game/chess_positions_as_fen.txt", "w") as file:
+        return file.write(fen_position)
+
+
 def get_stockfish_engine() -> Stockfish:
     root_path = getenv_or_raise_runtime("ROOT_PATH")
     return Stockfish(f"{root_path}/stockfish/stockfish")
@@ -65,7 +71,12 @@ def move_and_evaluate(
     stockfish: Stockfish, move: str
 ) -> typing.Literal["illegal", "white", "black", "stale", "move"]:
     inital_fen_position = stockfish.get_fen_position()
-    stockfish.make_moves_from_current_position([move])
+
+    try:
+        stockfish.make_moves_from_current_position([move])
+    except ValueError:
+        return "illegal"
+
     new_fen_position = stockfish.get_fen_position()
 
     if inital_fen_position == new_fen_position:
@@ -98,7 +109,7 @@ def print_decision(decision: typing.Literal["white", "black", "stale", "illegal"
 def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(
-        description="Apply a chess move using the Stockfish Python wrapper (without python-chess)."
+        description="Apply a chess move using the Stockfish Python wrapper. Current game state is saved in chess_positions_as_fen.txt file."
     )
     parser.add_argument(
         "move", type=str, help="Chess move in UCI notation (e.g., e2e4)"
@@ -110,20 +121,23 @@ def main():
     fen_position = get_current_fen_position()
     stockfish.set_fen_position(fen_position)
 
-    # Apply the move provided by the user
     users_move = args.move
     evaluation_decision = move_and_evaluate(stockfish=stockfish, move=users_move)
+    print(stockfish.get_board_visual())
     if evaluation_decision != "move":
         print_decision(decision=evaluation_decision)
         return
 
     stockfish_move = stockfish.get_best_move()
     evaluation_decision = move_and_evaluate(stockfish=stockfish, move=stockfish_move)
+    print(stockfish.get_board_visual())
+
     if evaluation_decision != "move":
         print_decision(decision=evaluation_decision)
         return
 
-    print(stockfish_move)
+    print(f"black move: {stockfish_move}")
+    update_current_fen_position(stockfish.get_fen_position())
 
 
 if __name__ == "__main__":
